ea9fabf8a0c04403ee6c8bd7bfbdf16fe3c48bcb
Martin Sternberg <sternberg@coll.mpg.de>
2020-04-20 13:21:17 +0200

    added the pages, we will need

--- bertrand_oligopoly/pages.py
+++ bertrand_oligopoly/pages.py
@@ -6,6 +6,32 @@ from .models import Constants
 class MyPage(Page):
     pass
 
+class Introduction(Page):
+    pass
+
+class Control_questions(Page):
+    pass
+
+class Start(Page):
+    pass
+
+class Round(Page):
+    pass
+
+class Decision(Page):
+    pass
+
+class Period_Result(Page):
+    pass
+
+class Round_Result(Page):
+    pass
+
+class Question_Algo (Page):
+    pass
+
+class Demographics(Page):
+    pass
 
 class ResultsWaitPage(WaitPage):
     def after_all_players_arrive(self):
